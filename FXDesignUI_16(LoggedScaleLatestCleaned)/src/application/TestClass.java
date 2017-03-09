5f54348bb3f1ad11094b5cf849e05decf1f0787c
SangappaG <SangappaG@Atindriya1>
2017-03-09 16:12:25 +0530

    Add Test

---
+++ FXDesignUI_16(LoggedScaleLatestCleaned)/src/application/TestClass.java
@@ -0,0 +1,36 @@
+//*H============================================================================
+//*M                      ATHINDRIYA SYSTEMS RESTRICTED
+//*H============================================================================
+//*H
+//*S  $Id: $
+//*H
+//*C  COPYRIGHT
+//*C  This software is copyrighted. It is the property of Athindriya Systems 
+//*C  India which reserves all right and title to it. It must not be
+//*C  reproduced, copied, published or released to third parties nor may the
+//*C  content be disclosed to third parties without the prior written consent
+//*C  of Athindriya Systems India. Offenders are liable to the payment of
+//*C  damages. All rights reserved in the event of the granting of a patent or
+//*C  the registration of a utility model or design.
+//*C  (c) Athindriya Systems India 2015
+//*H
+//*H  Created: 09-Mar-2017
+//*H
+//*H  @author....: $Author: $Suraj
+//*H  @date......: $Date: $
+//*H  @version...: $Rev: $1.0
+//*H  @path......: $URL: $
+//*H
+//*H============================================================================
+
+package application;
+
+/**
+ * Defines the class TestClass.
+ * @author SangappaG
+ * @version $Revision: $.
+ * @since 09-Mar-2017
+ */
+public class TestClass {
+
+}
